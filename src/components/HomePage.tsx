a382636393bd26532af8937c9c0e5fff11a7a3dd
Security2431 <Artem2431@gmail.com>
2019-08-23 21:07:42 +0200

    feat: render add method

--- src/components/HomePage.tsx
+++ src/components/HomePage.tsx
@@ -4,6 +4,8 @@ import getTeamArticlesDescription from '../utils/team-articles'
 import Button from './Button'
 
 class HomePage extends React.Component {
+  coolAdd = R.curry((x: number, y: number) => x + y)
+
   firstToUpper = (text: string): string => {
     const str = text.toString()
     const capitalizedStr = str[0].toUpperCase() + str.slice(1)
@@ -11,16 +13,24 @@ class HomePage extends React.Component {
     return capitalizedStr
   }
 
-  add = (...args: number[]): any => R.add(args[0], args[1])
-
   render() {
     const buttonText = this.firstToUpper('hello world')
+    const summ = `coolAdd(3, 4) ~> ${this.coolAdd(3, 4)}`
+    const currySumm = `coolAdd(3)(4) ~> ${this.coolAdd(3)(4)}`
 
     return (
       <main>
-        <pre>{getTeamArticlesDescription(1)}</pre>
-        <pre>{getTeamArticlesDescription(2)}</pre>
-        <Button>{buttonText}</Button>
+        <section>
+          <p>{summ}</p>
+          <p>{currySumm}</p>
+        </section>
+        <section>
+          <pre>{getTeamArticlesDescription(1)}</pre>
+          <pre>{getTeamArticlesDescription(2)}</pre>
+        </section>
+        <section>
+          <Button>{buttonText}</Button>
+        </section>
       </main>
     )
   }
